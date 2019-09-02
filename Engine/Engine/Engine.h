5547d305ee119747dec0554cb08b6772e4d077f5
HaijianLiu <haijanliu@gmail.com>
2019-09-03 00:52:23 +0900

    first commit

---
+++ Engine/Engine/Engine.h
@@ -0,0 +1,19 @@
+//
+//  Engine.h
+//  Engine
+//
+//  Created by haijian on 2019/09/03.
+//  Copyright © 2019 haijian. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Engine.
+FOUNDATION_EXPORT double EngineVersionNumber;
+
+//! Project version string for Engine.
+FOUNDATION_EXPORT const unsigned char EngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Engine/PublicHeader.h>
+
+
